6c55acf1c9e775c938b73f948aeed17722f16e48
Masaaki618 <masaaki_618@icloud.com>
2023-01-25 21:50:09 +0900

    refactor: エラーメッセージの言語に依存しない記載に修正 #30

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,19 +13,19 @@ RSpec.describe Task, type: :model do
       it 'nameが空の場合無効であること' do
         task = build(:task, name: nil)
         expect(task.valid?).to be false
-        expect(task.errors[:name]).to include('を入力してください')
+        expect(task.errors).to be_added(:name, :blank)
       end
 
       it 'nameの文字数が30文字以上の場合無効であること' do
         task = build(:task, name: 'a' * 31)
         expect(task.valid?).to be false
-        expect(task.errors[:name]).to include('は30文字以内で入力してください')
+        expect(task.errors).to be_added(:name, :too_long, count: 30)
       end
 
       it 'nameが既に登録されていたら無効であること' do
         task = build(:task, name: create_task.name)
         expect(task.valid?).to be false
-        expect(task.errors[:name]).to include('はすでに存在します')
+        expect(task.errors).to be_added(:name, :taken)
       end
 
       it 'nameに「,」が含まれていたら無効であること' do
