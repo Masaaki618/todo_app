c5ca871cf99a32b1e6b4b0016e859c48d88ca7f7
Mizuki <104258415+mizuki-28@users.noreply.github.com>
2023-01-26 10:08:25 +0900

    fix: エラーメッセージ表示のため修正 #41

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,9 +23,12 @@ class TasksController < ApplicationController
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク 「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
