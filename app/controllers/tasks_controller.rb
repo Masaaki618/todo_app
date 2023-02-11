05bfdac6a5dd332577e176503580a38f7102875d
Mizuki <104258415+mizuki-28@users.noreply.github.com>
2023-02-11 21:00:23 +0900

    fix: コンフリクトの解消 #47

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   skip_before_action :logged_in_user
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.recent
   end
 
   def new
@@ -30,15 +31,12 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_url, success: "タスク 「#{@task.name}」を更新しました。"
     else
@@ -47,7 +45,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_url, success: "タスク「#{@task.name}」を削除しました。"
   end
@@ -56,5 +53,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
