59bed66c1592f59a1320e7338dca09a473c394f4
daisuke721 <d.magome.07@gmail.com>
2023-02-15 22:36:34 +0900

    test: system specにてtimeoutが生じる #52

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
+  let(:task) { create(:task) }
+
+  before do
+    visit root_path
+  end
+
+  it '作成したタスクが表示されること' do
+    expect(page).to have_content task.name
+  end
 end
