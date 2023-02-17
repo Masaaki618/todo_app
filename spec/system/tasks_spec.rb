b138360ec64ea319765de7897a94402ab5004a6b
daisuke721 <d.magome.07@gmail.com>
2023-02-17 22:48:21 +0900

    test: ログイン機能の共通化 #52

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  let(:task) { create(:task) }
-
-  before do
-    visit root_path
-  end
-
-  it '作成したタスクが表示されること' do
-    expect(page).to have_content task.name
-  end
 end
