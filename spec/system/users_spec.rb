86fb6aa273ae0f71339c0b577509abbefc2e5712
daisuke721 <d.magome.07@gmail.com>
2023-02-18 00:42:00 +0900

    test: ログイン機能のテスト追加 #52

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'User機能' do
+    describe 'ユーザー登録' do
+      before do
+        visit sign_up_path
+      end
+      context 'フォームの入力値が正常' do
+        it 'ユーザーの新規作成が成功すること' do
+          fill_in 'user_name', with: 'テストユーザー'
+          fill_in 'user_email', with: 'test@example.com'
+          fill_in 'password', with: 'password'
+          fill_in 'user_password_confirmation', with: 'password'
+          click_button '登録する'
+          expect(current_path).to eq root_path
+        end
+      end
+
+      context 'メールアドレスの未入力' do
+        it 'ユーザーの新規作成が失敗すること' do
+          fill_in 'user_name', with: 'テストユーザー'
+          fill_in 'user_email', with: nil
+          fill_in 'password', with: 'password'
+          fill_in 'user_password_confirmation', with: 'password'
+          click_button '登録する'
+          expect(current_path).to eq sign_up_path
+        end
+      end
+    end
+
+    describe 'ログイン後' do
+      it 'ログアウトが成功すること' do
+        login(user)
+        expect(page).to have_link 'ログアウト', href: logout_path
+      end
+    end
+  end
+end
