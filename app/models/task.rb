249cd981254cf81bd87e3475621f59ac41ee25c9
Mizuki <104258415+mizuki-28@users.noreply.github.com>
2023-02-19 00:44:46 +0900

    fix: rubocop指摘箇所修正 #55

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,11 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
-  def self.ransackable_attributes(auth_object = nil)
-    %w[name created_at]
+  def self.ransackable_attributes(auth_object: nil)
+    %w(name created_at)
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(auth_object: nil)
     []
   end
 
