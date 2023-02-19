7379c5ded5f9a1e5a326ed8e69e7bec39daae925
Masaaki618 <masaaki_618@icloud.com>
2023-02-19 23:52:26 +0900

    refactor: 画像の有無で処理を変更 #56

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
-
+    binding.pry
     if params[:back].present?
       render :new
       return
