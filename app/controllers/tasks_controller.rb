5ecee10ae8f676968a4f57989f69d68e32e00bd5
Masaaki618 <masaaki_618@icloud.com>
2023-02-20 22:17:24 +0900

    add: task編集から画像を追加できるように #56

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,6 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
-    binding.pry
     if params[:back].present?
       render :new
       return
